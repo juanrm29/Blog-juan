f8c6eba7f70491aed842eb8ec40303d9017b13d6
juanrm29 <juanmaulana29@gmail.com>
2024-01-28 02:09:04 +0800

    DD

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose.connect(process.env.DB_LOCATION, {
 // setting up s3 bucket
 const s3 = new aws.S3({
     region: process.env.AWS_BUCKET_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS
 })
 
